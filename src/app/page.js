cad72e4d53d654156a8ecc9c8e4544b4ade627cf
Hoccyy <94509399+Hoccyy@users.noreply.github.com>
2023-03-18 23:13:51 -0400

    Layout made

--- src/app/page.js
+++ src/app/page.js
@@ -1,90 +1,59 @@
+'use client';
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
+//import { use } from 'react'
+import { useState } from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
+const text = 'Type here to start...'
+
+var output = "Tra";
 
 export default function Home() {
+  //const [text, setText] = useState('Type here to start...');
+
+  function handleClick() {
+    return;
+  }
+
+  const mainTranslation = (event) => {
+    let userInput = ( event.target.value );
+    let ct = 0;
+    let translationMode = false;
+    let tempTranslation = "";
+
+    //Dictionaries
+    var morseAlphabet = {" ": "\/", "@": ".--.-.", "a": " .- ", "b": "  -... ", "c": " -.-. ", "d": " -.. ", "e": " . ", "f": " ..-. ", "g": " --. ", "h": " .... ", "i": " .. ", "j": " .--- ", "k": " -.- ", "l": " .-.. ", "m": " -- ", "n": " -. ", "o": " --- ", "p": " .--. ", "q": " --.- ", "r": " .-. ", "s": " ... ", "t": " - ", "u": " ..- ", "v": " ...- ", "w": " .-- ", "x": " -..- ", "y": " -.-- ", "z": " --.. ", "1": " .---- ", "2": " ..--- ", "3": " ...-- ", "4": " ....- ", "5": " ..... ", "6": " -.... ", "7": " --... ", "8": " ---.. ", "9": " ----. ", "0": " ----- ", ".": " .-.-.- ", ",": " --..-- ", "?": " ..--.. ", "!": " ..--. ", ":": " ---... ", "\"": " .-..-. ", "\'": " .----. ", "=": " -...- "};
+
+  
+    
+    if (/^[0-9a-zA-Z]+$/.test(userInput[0])) {
+      translationMode = true;
+    }
+    //alert(translationMode)
+    if (translationMode){
+      
+      for (var v = 0; v < userInput.length; v++){
+
+        tempTranslation += morseAlphabet[userInput[v]];
+      }
+      output += "dawg";
+      alert(output);
+    }
+
+  }
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <ul id={styles.languages}>
+        <li className={styles.description}>English</li>
+        <li className={styles.description}> </li>
+        <li className={styles.description}>MorseCode</li>
+      </ul>
+      <div className={styles.textArea}>
+        <textarea id={styles.InputBox} onClick={handleClick} onChange={mainTranslation} placeHolder={text} />
+        <textarea id={styles.OutputBox} disabled value={output}></textarea>
       </div>
     </main>
   )
