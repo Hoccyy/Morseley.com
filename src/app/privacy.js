7e461c6d6b9aea985292328f4081b2638393680c
Hoccyy <94509399+Hoccyy@users.noreply.github.com>
2023-03-22 16:56:35 -0400

    Create privacy.js

---
+++ src/app/privacy.js
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+
+const PrivacyPolicy = () => {
+  return (
+    <>
+      <Head>
+        <title>Privacy Policy</title>
+      </Head>
+
+      <h1>Privacy Policy</h1>
+
+      <h2>This is just a personal project made for practice and I don't intend to collect any data.</h2>
+      <h2>Contact me <a href='mailto:hoccyy13@gmail.com?subject=Morseley%20Translator'>here</a> if you have any concerns.</h2>
+    </>
+  );
+};
+
+export default PrivacyPolicy;
