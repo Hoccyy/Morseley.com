39b9b4af7bd768466d423dcd030cd79d063ec645
Hoccyy <94509399+Hoccyy@users.noreply.github.com>
2023-04-03 18:40:33 -0400

    Structure changes

--- src/app/page.js
+++ src/app/page.js
@@ -121,11 +121,8 @@ export default function Home() {
     }
   }
   return (
-    <main className={styles.main}>
+    <main className={styles.main_}>
       <Analytics/>
-      <head>
-        <script async src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9022203058839959' crossOrigin='anonymous'></script>
-      </head>
       <ul id={styles.languages}>
         <li className={styles.description}>English</li>
         <li className={styles.description}></li>
--- src/app/privacy.js
+++ src/app/privacy.js
@@ -8,7 +8,7 @@ const PrivacyPolicy = () => {
       </Head>
       <h1>Privacy Policy</h1>
 
-      <h2>This is just a personal project made for practice and I don't intend to collect any data.</h2>
+      <h2>This is just a personal project made for practice and I don't intend to collecting any data.</h2>
       <h2>Contact me <a href='mailto:hoccyy13@gmail.com?subject=Morseley%20Translator'>here</a> if you have any concerns.</h2>
     </>
   );
